d970c4aeb7f711b1ee3615bd63afa0750bb710ed
VayuDev <vayudev@protonmail.com>
2022-01-02 00:58:36 +0100

    Fix missing clearing of ApplicationState::mCurrentRWHolderOfMMutex

--- src/ApplicationState.cpp
+++ src/ApplicationState.cpp
@@ -262,6 +262,7 @@ void ApplicationState::cleanup() {
     // that need to be deleted.
     if(mClientsWereLoggedOutSinceLastCleanup) {
         lock.unlock();
+        mCurrentRWHolderOfMMutex = std::thread::id();
         mClientManager.suspendAllThreads();
         lock.lock();
         mCurrentRWHolderOfMMutex = std::this_thread::get_id();
@@ -432,6 +433,7 @@ void ApplicationState::publish(std::string&& topic, std::vector<uint8_t>&& msg, 
     std::shared_lock<std::shared_mutex> lock{ mMutex };
     publishNoLockNoRetain(topic, msg, qos, retain);
     lock.unlock();
+    mCurrentRWHolderOfMMutex = std::thread::id();
     if(retain == Retain::Yes) {
         // we aren't allowed to call requestChange from another thread while holding a lock, so we need to do it here
         requestChange(ChangeRequestRetain{std::move(topic), std::move(msg)});
